0138a149b97624595d50773a434b9fa895fee174
ilyahorski <72282557+ilyahorski@users.noreply.github.com>
2024-10-25 23:10:30 +0200

    Update route.js

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -13,6 +13,7 @@ export const GET = async (request, { params }) => {
         'Content-Type': 'application/json',
         'Cache-Control': 'no-store',
       },
+    });
   } catch (error) {
     return new Response('Failed to fetch users', { status: 500 });
   }
